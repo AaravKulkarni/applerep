4f9dcc9d8bd2866cbd66874445577a00422ba5d1
AaravKulkarni <88075592+AaravKulkarni@users.noreply.github.com>
2022-11-20 14:58:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+address= "";
+price="";
+creditcardno="";
+debitcardno="";
+method="";
+contactno="";
+itemname="";
+
+
+
+
+
+
+function buy(){
+    window.location.href="payment.html";
+}
+
+
+
+
+function back(){
+    window.location.href="index.html";
+}
+
+
+function pay(){
+   document.getElementById("confirmation").innerHTML = "Your Order Has Been Recieved And Will Be Shipped Shortly";
+
+}
